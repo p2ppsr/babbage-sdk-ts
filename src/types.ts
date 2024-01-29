3d4bb566ac4fbf3b06560834f783e9b029815747
Tone <tone@kizmet.org>
2024-01-29 15:31:04 -0500

    cwi-base depends on babbage-sdk-ts

--- src/types.ts
+++ src/types.ts
@@ -58,6 +58,29 @@ export interface MapiResponseApi {
   mimetype?: string // "application/json", "image/jpeg"
 }
 
+/**
+ * Simplest case of an envelope is a `rawTx` and merkle `proof` that ties the transaction to a known block header.
+ * This will be the case for any sufficiently old transaction.
+ *
+ * If the transaction has been mined but for some reason the block headers may not be known, an array of `headers` linking
+ * known headers to the one needed by the `proof` may be provided. They must be in height order and need to overlap
+ * a known header.
+ *
+ * If the transaction has not been minded yet but it has been submitted to one or more miners then the mapi responses
+ * received, proving that specific miners have received the transaction for processing, are included in the
+ * mapiResponses array.
+ * Note that the miner reputations must be checked to give weight to these responses.
+ *
+ * Additionally, when the transaction hasn't been mined or a `proof` is unavailable and mapi responses proving miner
+ * acceptance are unavailable, then all the transactions providing inputs can be submitted in an inputs object.
+ *
+ * The keys of the inputs object are the transaction hashes (txids) of each of the input transactions.
+ * The value of each inputs object property is another envelope object.
+ *
+ * References:
+ * Section 2 of https://projectbabbage.com/assets/simplified-payments.pdf
+ * https://gist.github.com/ty-everett/44b6a0e7f3d6c48439f9ff26068f8d8b
+ */
 export interface EnvelopeApi extends EnvelopeEvidenceApi {
   /**
      * For root nodes only.
@@ -227,6 +250,8 @@ export interface ProveCertificateResult extends CertificateApi {
   keyring: Record<string, string>
 }
 
+export type TransactionStatusApi = 'completed' | 'failed' | 'unprocessed' | 'sending' | 'unproven' | 'unsigned'
+
 export interface ListActionsTransaction {
   /**
      * The transaction ID
@@ -239,7 +264,7 @@ export interface ListActionsTransaction {
   /**
      * The current state of the transaction. Common statuses are `completed` and `unproven`.
      */
-  status: string
+  status: TransactionStatusApi
   /**
      * The Paymail handle of the person who sent the transaction
      */
