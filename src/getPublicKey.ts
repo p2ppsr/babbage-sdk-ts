de8ce7d87644354141cd230211573a2e4b1175bd
Tone <tone@kizmet.org>
2024-03-06 01:31:06 -0500

    getPublicKey: protocolID and keyID now optional

--- src/getPublicKey.ts
+++ src/getPublicKey.ts
@@ -5,9 +5,8 @@ import connectToSubstrate from './utils/connectToSubstrate'
  * Returns the public key. If identityKey is specified, returns the current user's identity key. If a counterparty is specified, derives a public key for the counterparty.
  *
  * @param {Object} args All parameters are passed in an object.
- * @param {Array|string} [args.protocolID] Specify an identifier for the protocol under which this operation is being performed.
- * @param {string} [args.keyID] An identifier for retrieving the public key used. This can be used to prevent key re-use, even when the same user is using the same protocol to perform actions.
- * @param {string} [args.description] Describe the high-level operation being performed, so that the user can make an informed decision if permission is needed.
+ * @param {Array|string} [args.protocolID] Optional. Specify an identifier for the protocol under which this operation is being performed.
+ * @param {string} [args.keyID] Optional. An identifier for retrieving the public key used. This can be used to prevent key re-use, even when the same user is using the same protocol to perform actions.
  * @param {string} [args.privileged=false] This indicates whether the privileged keyring should be used, as opposed to the primary keyring.
  * @param {string} [args.identityKey=false] If true, the identity key will be returned, and no key derivation will be performed
  * @param {string} [args.reason] The reason for requiring access to the user's privilegedKey
@@ -17,8 +16,8 @@ import connectToSubstrate from './utils/connectToSubstrate'
 * @returns {Promise<string>} The user's public key
  */
 export async function getPublicKey(args: {
-  protocolID: ProtocolID,
-  keyID: string,
+  protocolID?: ProtocolID,
+  keyID?: string,
   privileged?: boolean, // = false,
   identityKey?: boolean, // = false,
   reason?: string, // = 'No reason provided.',
