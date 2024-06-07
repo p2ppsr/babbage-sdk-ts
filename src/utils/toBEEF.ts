e845adf03d0df85a98f270d412d2bac03bf4fecf
Tone Engel <tone@kizmet.org>
2024-06-07 06:17:13 -0400

    toBEEF.ts bug fixes, initial test passing

--- src/utils/toBEEF.ts
+++ src/utils/toBEEF.ts
@@ -68,35 +68,35 @@ export function convertProofToMerklePath(txid: string, proof: TscMerkleProofApi)
     }
     const blockHeight = proof.height
     const treeHeight = proof.nodes.length
-    const path: {
+    type Leaf = {
         offset: number
         hash?: string
         txid?: boolean
-        duplicate: boolean
-    }[][] = Array(treeHeight).fill(0).map(() => ([]))
+        duplicate?: boolean
+    }
+    const path: Leaf[][] = Array(treeHeight).fill(0).map(() => ([]))
     let index = proof.index
     for (let level = 0; level < treeHeight; level++) {
         const node = proof.nodes[level]
         const isOdd = index % 2 === 1
-        const offset = level > 0 ? index : isOdd ? index - 1 : index + 1
-        const leaf = {
-            offset,
-            hash: node === '*' ? undefined : node,
-            txid: false,
-            duplicate: node === '*'
+        const offset = isOdd ? index - 1 : index + 1
+        const leaf: Leaf = { offset }
+        if (node === '*' || (level === 0 && node === txid)) {
+            leaf.duplicate = true
+        } else {
+            leaf.hash = node
         }
         path[level].push(leaf)
         if (level === 0) {
-            const txidLeaf = {
+            const txidLeaf: Leaf = {
                 offset: proof.index,
                 hash: txid,
                 txid: true,
-                duplicate: false
             }
             if (isOdd) {
-                path[0].unshift(txidLeaf)
-            } else {
                 path[0].push(txidLeaf)
+            } else {
+                path[0].unshift(txidLeaf)
             }
         }
         index = index >> 1
