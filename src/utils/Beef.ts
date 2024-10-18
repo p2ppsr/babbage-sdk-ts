5acc50e981a8c435e866f87ea06f781be7458590
Tone Engel <tone@kizmet.org>
2024-10-18 11:41:22 -0400

    Add BeefParty

--- src/utils/Beef.ts
+++ src/utils/Beef.ts
@@ -4,118 +4,6 @@ import { asString, doubleSha256BE } from "./Helpers";
 export const BEEF_MAGIC = 4022206465 // 0100BEEF in LE order
 export const BEEF_MAGIC_TXID_ONLY_EXTENSION = 4022206465 // 0100BEEF in LE order
 
-/**
- * A single bitcoin transaction associated with a `Beef` validity proof set.
- * 
- * Simple case is transaction data included directly, either as raw bytes or fully parsed data, or both.
- * 
- * Also supports 'known' transactions which are represented by just their txid.
- * It can be assumed that an external party already has validity proof for this transaction.
- */
-export class BeefTx {
-    _bumpIndex?: number
-    _tx?: Transaction
-    _rawTx?: number[]
-    _txid?: string
-    inputTxids: string[] = []
-    degree: number = 0
-
-    get bumpIndex() : number | undefined { return this._bumpIndex}
-
-    set bumpIndex(v: number | undefined) {
-        this._bumpIndex = v
-        this.updateInputTxids()
-    }
-
-    get hasProof() : boolean {
-        return this._bumpIndex !== undefined
-    }
-
-    get isTxidOnly() : boolean {
-        return !!this._txid && !this._rawTx && !this._tx
-    }
-
-    get txid() {
-        if (this._txid) return this._txid
-        if (this._tx) return this._txid = this._tx.id('hex')
-        if (this._rawTx) return this._txid = asString(doubleSha256BE(Buffer.from(this._rawTx)))
-        throw new Error('Internal')
-    }
-
-    get tx() {
-        if (this._tx) return this._tx
-        if (this._rawTx) return this._tx = Transaction.fromBinary(this._rawTx)
-        return undefined
-    }
-
-    get rawTx() {
-        if (this._rawTx) return this._rawTx
-        if (this._tx) return this._rawTx = this._tx.toBinary()
-        return undefined
-    }
-
-    constructor (tx: Transaction | number[] | string, bumpIndex?: number) {
-        if (typeof tx === 'string') {
-            this._txid = tx
-        } else {
-            if (Array.isArray(tx)) {
-                this._rawTx = tx
-            } else {
-                this._tx = <Transaction>tx
-            }
-        }
-        this.bumpIndex = bumpIndex
-        this.updateInputTxids()
-    }
-
-    private updateInputTxids() {
-        if (this.hasProof || !this.tx)
-            // If we have a proof, or don't have a parsed transaction
-            this.inputTxids = []
-        else {
-            const inputTxids = {};
-            for (const input of this.tx.inputs)
-                inputTxids[input.sourceTXID!] = true;
-            this.inputTxids = Object.keys(inputTxids);
-        }
-    }
-
-    toWriter(writer: Utils.Writer) : void {
-        if (this.isTxidOnly) {
-            // Encode just the txid of a known transaction using the txid
-            writer.writeUInt32LE(BEEF_MAGIC_TXID_ONLY_EXTENSION)
-            writer.writeReverse(Utils.toArray(this._txid, 'hex'))
-        } else if (this._rawTx)
-            writer.write(this._rawTx)
-        else if (this._tx)
-            writer.write(this._tx.toBinary())
-        else
-            throw new Error('a valid serialized Transaction is expected')
-        if (this.bumpIndex === undefined) {
-            writer.writeUInt8(0)
-        } else {
-            writer.writeUInt8(1)
-            writer.writeVarIntNum(this.bumpIndex)
-        }
-    }
-
-    static fromReader (br: Utils.Reader): BeefTx {
-        let tx: Transaction | number[] | string | undefined = undefined
-        const version = br.readUInt32LE()
-        if (version === BEEF_MAGIC_TXID_ONLY_EXTENSION) {
-            // This is the extension to support known transactions
-            tx = Utils.toHex(br.readReverse(32))
-        } else {
-            br.pos -= 4 // Unread the version...
-            tx = Transaction.fromReader(br)
-        }
-        const bumpIndex = br.readUInt8() ? br.readVarIntNum() : undefined
-        const beefTx = new BeefTx(tx, bumpIndex)
-        return beefTx
-    }
-
-}
-
 /*
  * BEEF standard: BRC-62: Background Evaluation Extended Format (BEEF) Transactions
  * https://github.com/bitcoin-sv/BRCs/blob/master/transactions/0062.md
@@ -550,6 +438,30 @@ export class Beef {
     }
 
     /**
+     * @returns a shallow copy of this beef
+     */
+    clone() : Beef {
+        const c = new Beef()
+        c.bumps = Array.from(this.bumps)
+        c.txs = Array.from(this.txs)
+        return c
+    }
+
+    /**
+     * Ensure that all the txids in `knownTxids` are txidOnly
+     * @param knownTxids 
+     */
+    trimKnownTxids(knownTxids: string[]) {
+        for (let i = 0; i < this.txs.length; i++) {
+            const tx = this.txs[i]
+            if (!tx.isTxidOnly && -1 < knownTxids.indexOf(tx.txid)) {
+                this.txs[i] = new BeefTx(tx.txid)
+            }
+        }
+        // TODO: bumps could be trimmed to eliminate unreferenced proofs.
+    }
+
+    /**
      * @returns Summary of `Beef` contents as multi-line string.
      */
     toLogString() : string {
@@ -580,14 +492,198 @@ export class Beef {
     }
 }
 
+/**
+ * A single bitcoin transaction associated with a `Beef` validity proof set.
+ * 
+ * Simple case is transaction data included directly, either as raw bytes or fully parsed data, or both.
+ * 
+ * Supports 'known' transactions which are represented by just their txid.
+ * It is assumed that intended consumer of this beef already has validity proof for such a transaction,
+ * which they can merge if necessary to create a valid beef.
+ */
+export class BeefTx {
+    _bumpIndex?: number
+    _tx?: Transaction
+    _rawTx?: number[]
+    _txid?: string
+    inputTxids: string[] = []
+    degree: number = 0
+
+    get bumpIndex() : number | undefined { return this._bumpIndex}
+
+    set bumpIndex(v: number | undefined) {
+        this._bumpIndex = v
+        this.updateInputTxids()
+    }
+
+    get hasProof() : boolean {
+        return this._bumpIndex !== undefined
+    }
+
+    get isTxidOnly() : boolean {
+        return !!this._txid && !this._rawTx && !this._tx
+    }
+
+    get txid() {
+        if (this._txid) return this._txid
+        if (this._tx) return this._txid = this._tx.id('hex')
+        if (this._rawTx) return this._txid = asString(doubleSha256BE(Buffer.from(this._rawTx)))
+        throw new Error('Internal')
+    }
+
+    get tx() {
+        if (this._tx) return this._tx
+        if (this._rawTx) return this._tx = Transaction.fromBinary(this._rawTx)
+        return undefined
+    }
+
+    get rawTx() {
+        if (this._rawTx) return this._rawTx
+        if (this._tx) return this._rawTx = this._tx.toBinary()
+        return undefined
+    }
+
+    /**
+     * @param tx If string, must be a valid txid. If `number[]` must be a valid serialized transaction.
+     * @param bumpIndex If transaction already has a proof in the beef to which it will be added.
+     */
+    constructor (tx: Transaction | number[] | string, bumpIndex?: number) {
+        if (typeof tx === 'string') {
+            this._txid = tx
+        } else {
+            if (Array.isArray(tx)) {
+                this._rawTx = tx
+            } else {
+                this._tx = <Transaction>tx
+            }
+        }
+        this.bumpIndex = bumpIndex
+        this.updateInputTxids()
+    }
+
+    private updateInputTxids() {
+        if (this.hasProof || !this.tx)
+            // If we have a proof, or don't have a parsed transaction
+            this.inputTxids = []
+        else {
+            const inputTxids = {};
+            for (const input of this.tx.inputs)
+                inputTxids[input.sourceTXID!] = true;
+            this.inputTxids = Object.keys(inputTxids);
+        }
+    }
+
+    toWriter(writer: Utils.Writer) : void {
+        if (this.isTxidOnly) {
+            // Encode just the txid of a known transaction using the txid
+            writer.writeUInt32LE(BEEF_MAGIC_TXID_ONLY_EXTENSION)
+            writer.writeReverse(Utils.toArray(this._txid, 'hex'))
+        } else if (this._rawTx)
+            writer.write(this._rawTx)
+        else if (this._tx)
+            writer.write(this._tx.toBinary())
+        else
+            throw new Error('a valid serialized Transaction is expected')
+        if (this.bumpIndex === undefined) {
+            writer.writeUInt8(0)
+        } else {
+            writer.writeUInt8(1)
+            writer.writeVarIntNum(this.bumpIndex)
+        }
+    }
+
+    static fromReader (br: Utils.Reader): BeefTx {
+        let tx: Transaction | number[] | string | undefined = undefined
+        const version = br.readUInt32LE()
+        if (version === BEEF_MAGIC_TXID_ONLY_EXTENSION) {
+            // This is the extension to support known transactions
+            tx = Utils.toHex(br.readReverse(32))
+        } else {
+            br.pos -= 4 // Unread the version...
+            tx = Transaction.fromReader(br)
+        }
+        const bumpIndex = br.readUInt8() ? br.readVarIntNum() : undefined
+        const beefTx = new BeefTx(tx, bumpIndex)
+        return beefTx
+    }
+
+}
+
+/**
+ * When beefs are used to exchange transaction validity data with more than one external party.
+ * 
+ * Keeps track of who knows what to reduce re-transmission of potentially large transactions.
+ */
 export class BeefParty extends Beef {
     /**
-     * keys are party identifiers, each must be unique
-     * values are arrays of txids for which we have evidence that the party already has the rawTx
+     * keys are party identifiers.
+     * values are records of txids with truthy value for which the party already has validity proof.
      */
-    knownTo: Record<string, string[]> = {}
+    knownTo: Record<string, Record<string, boolean>> = {}
 
-    constructor() {
+    /**
+     * 
+     * @param parties Optional array of initial unique party identifiers.
+     */
+    constructor(parties?: string[]) {
         super()
+        if (parties) {
+            for (const party of parties)
+                this.addParty(party)
+        }
+    }
+
+    /**
+     * @param party 
+     * @returns `true` if `party` has already beed added to this `BeefParty`.
+     */
+    isParty(party: string) {
+        const r = Object.keys(this.knownTo).includes(party)
+        return r
+    }
+
+    /**
+     * Adds a new unique party identifier to this `BeefParty`.
+     * @param party 
+     */
+    addParty(party: string) {
+        if (this.isParty(party))
+            throw new Error(`Party ${party} already exists.`)
+        this.knownTo[party] = {}
+    }
+
+    /**
+     * @param party 
+     * @returns Array of txids "known" to `party`.
+     */
+    getPartyKnownTxids(party: string) : string[] {
+        const knownTxids = this.knownTo[party]
+        if (!knownTxids)
+            throw new Error(`Party ${party} is unknown.`)
+        return Object.keys(knownTxids)
+    }
+
+    /**
+     * @param party 
+     * @returns trimmed beef of unknown transactions and proofs for `party`
+     */
+    getTrimmedBeefForParty(party: string) : Beef {
+        const knownTxids = this.getPartyKnownTxids(party)
+        const prunedBeef = this.clone()
+        prunedBeef.trimKnownTxids(knownTxids)
+        return prunedBeef
+    }
+
+    /**
+     * Make note of additional txids "known" to `party`.
+     * @param party unique identifier, added if new.
+     * @param knownTxids 
+     */
+    addKnownTxidsForParty(party: string, knownTxids: string[]) {
+        if (!this.isParty(party))
+            this.addParty(party)
+        const kts = this.knownTo[party]
+        for (const txid of knownTxids)
+            kts[txid] = true
     }
 }
